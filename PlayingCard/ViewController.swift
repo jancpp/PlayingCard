49b13f3d5814e763b172d5202475dc1ca84e212d
Jan Polzer <janman74@gmail.com>
2018-06-12 20:50:19 -0500

    Created Cards and deck

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Jan Polzer on 6/12/18.
+//  Copyright © 2018 Apps KC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // test: prinf 10 random cards in console
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
+    }
+
+}
+
